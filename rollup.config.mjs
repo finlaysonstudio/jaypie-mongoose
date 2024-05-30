721f2f5294c831731ef3c68682f0d708f108f05a
Adam Finlayson <amf@adamfinlayson.com>
2024-05-30 16:44:10 -0500

    fix: change output of rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,7 +6,7 @@ export default {
   input: "src/index.js", // Path to your main JavaScript file
   output: [
     {
-      file: "dist/jaypie-mongoose.cjs.js", // Output file for CommonJS
+      file: "dist/jaypie-mongoose.cjs", // Output file for CommonJS
       format: "cjs", // CommonJS format
     },
     {
